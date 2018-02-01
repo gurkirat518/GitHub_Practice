4ba5c3e345ff2cd9c375a569f1869a454930d89c
gurkirat518 <gurkirat@zwing.in>
2018-02-01 17:11:33 +0530

    modify main and added new activity

--- app/src/main/java/com/xyz/github_practice/Main2Activity.java
+++ app/src/main/java/com/xyz/github_practice/Main2Activity.java
@@ -3,15 +3,11 @@ package com.xyz.github_practice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    void add(){
-        int i=1+1;
-        int j=i+1;
+        setContentView(R.layout.activity_main2);
     }
 }
